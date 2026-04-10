51d2a235c75bc86a067a9b1db175bb53615f1ae4
AR-JUN-G <arjun07091102@gmail.com>
2026-04-11 01:09:14 +0530

    render backend url fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,12 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777/api" : "http://51.21.250.140:7777/api";
-export const SOCKET_URL = location.hostname === "localhost" ? "http://localhost:7777" : "http://51.21.250.140:7777";
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777/api" : "http://51.21.250.140:7777/api";
+// export const SOCKET_URL = location.hostname === "localhost" ? "http://localhost:7777" : "http://51.21.250.140:7777";
+
+
+
+export const BASE_URL = location.hostname === "localhost" 
+  ? "http://localhost:7777/api" 
+  : "https://connectifybe.onrender.com/api";
+
+export const SOCKET_URL = location.hostname === "localhost" 
+  ? "http://localhost:7777" 
+  : "https://connectifybe.onrender.com";
